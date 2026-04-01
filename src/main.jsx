37ceb7ce54af8f060401155b5316879da7c2860d
novomarketus-cell <novomarketus@gmail.com>
2026-04-01 14:36:29 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NovoMarket from "./novo-market-v2";
+import Admin from "./novo-admin-dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NovoMarket />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
